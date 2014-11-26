ca8362dcb7ce31510b7aaead7130a3bfa72bafdc
Christoph Schulze <chr_schulze@freenet.de>
2014-11-26 20:59:02 +0100

    receive data from view

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -1,9 +1,31 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.awt.*;
+import java.awt.Label;
 
 public class Menu {
 
+    public Slider fieldsSlider;
+    public Slider colorsSlider;
+    public ToggleGroup enemy;
+
+    @FXML
     public void startGame(ActionEvent actionEvent) {
+        double fieldsCount = fieldsSlider.getValue();
+        double colorsCount = colorsSlider.getValue();
+        String radioBtn = ((RadioButton) enemy.getSelectedToggle()).getId();
+        if (radioBtn.equals("playerRadBtn")){
+            System.out.println("chose multiplayer mode");
+        } else if (radioBtn.equals("kiRadBtn")) {
+            System.out.println("chose ki mode");
+        }
+
+        System.out.println("chose " + fieldsCount + " fields");
+        System.out.println("chose " + colorsCount + " different colors");
+
     }
 }
