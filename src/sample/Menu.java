2fcbcd373a354057f8cbcc8c7ea9286f0b30a3aa
doodzik <frederik.dudzik@gmail.com>
2014-11-27 12:29:47 +0100

    print field when new game is clicked

--- src/sample/Menu.java
+++ src/sample/Menu.java
@@ -2,7 +2,14 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 public class Menu {
 
@@ -11,7 +18,7 @@ public class Menu {
     public ToggleGroup enemy;
 
     @FXML
-    public void startGame(ActionEvent actionEvent) {
+    public void startGame(ActionEvent actionEvent) throws Exception{
         double fieldsCount = fieldsSlider.getValue();
         double colorsCount = colorsSlider.getValue();
         String radioBtn = ((RadioButton) enemy.getSelectedToggle()).getId();
@@ -20,6 +27,13 @@ public class Menu {
         } else if (radioBtn.equals("kiRadBtn")) {
             System.out.println("chose ki mode");
         }
+
+        Node node=(Node) actionEvent.getSource();
+        Stage stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("field.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
 
         System.out.println("chose " + fieldsCount + " fields");
         System.out.println("chose " + colorsCount + " different colors");
