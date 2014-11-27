9822ec107dfec033bbdf08edea4c35851be36280
doodzik <frederik.dudzik@gmail.com>
2014-11-27 12:36:37 +0100

    add new game button

--- src/sample/Field.java
+++ src/sample/Field.java
@@ -1,11 +1,22 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * Created by dudzik on 26.11.14.
  */
 public class Field {
-    public void newGame(ActionEvent actionEvent) {
+    public void newGame(ActionEvent actionEvent) throws Exception{
+        Node node=(Node) actionEvent.getSource();
+        Stage stage=(Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
